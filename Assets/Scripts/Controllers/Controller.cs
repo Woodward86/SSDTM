68335d78c44a690fe91644c2707b0fecab0abc75
Woodward86 <delwoodward86@gmail.com>
2019-06-08 14:07:07 -0700

    set up basic collisions,Basic movement, facing direction, jump and double jump

--- Assets/Scripts/Controllers/Controller.cs
+++ Assets/Scripts/Controllers/Controller.cs
@@ -13,10 +13,20 @@ public class Controller : MonoBehaviour
     protected PlayerStats stats;
 
     [Header("Movement Settings")]
-    protected Vector3 velocity;
-
+    public float jumpTime;
     public float fallGravityMultiplier = 5.0f;
-    public float lowJumpGravityMultiplier = 3.0f;
+    public float lowJumpMultiplier = 3.0f;
+
+    protected float moveInput;
+    protected float facingDirection;
+    protected bool jumpRequest;
+    protected int jumpCounter;
+    protected float jumpRequestTime;
+    protected float jumpTimeCounter;
+    protected bool isGrounded;
+    protected bool isContactRight;
+    protected bool isContactLeft;
+    protected bool isContactAbove;
 
 
     private void OnEnable()
@@ -35,26 +45,49 @@ public class Controller : MonoBehaviour
 
     void Update()
     {
-        
+        GetInput();
+    }
+
+
+    protected virtual void GetInput()
+    {
+
     }
 
 
     private void FixedUpdate()
     {
-        JumpModfier();
-
-        //Vector3 deltaPosition = rb.velocity * Time.deltaTime;
-
-        //Vector3 move = Vector3.up * deltaPosition.y;
-
-        //Movement(move);
-        Debug.DrawRay(rb.position, rb.velocity);
+        Movement();
+        Debug.DrawRay(rb.position, rb.velocity, Color.red);
     }
 
 
-    void Movement(Vector3 move)
+    void Movement()
     {
-        rb.position = rb.position + move;
+        CollisionTests();
+        JumpModfier();
+
+        //TODO walking down and up slopes needs to be fixed so you stick to the ground might need to change this over to rb.position
+        rb.velocity = new Vector3(moveInput * stats.walkSpeed.GetValue(), rb.velocity.y, rb.velocity.z);
+
+        if (jumpRequest)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, stats.jumpHeight.GetValue(), rb.velocity.z);
+        }
+    }
+
+
+    void CollisionTests()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, coll.bounds.extents.y + 0.1f);
+        isContactAbove = Physics.Raycast(transform.position, Vector3.up, coll.bounds.extents.y + 0.1f);
+        isContactRight = Physics.Raycast(transform.position, Vector3.right, coll.bounds.extents.x + 0.1f);
+        isContactLeft = Physics.Raycast(transform.position, Vector3.left, coll.bounds.extents.x + 0.1f);
+        
+        if(isGrounded)
+        {
+            jumpCounter = 1;
+        }
     }
 
 
@@ -64,10 +97,17 @@ public class Controller : MonoBehaviour
         {
             rb.velocity += Physics.gravity * (fallGravityMultiplier - 1) * Time.deltaTime;
         }
-//         else if (rb.velocity.y > 0 && !jumpRequest)
-//         {
-//             rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpGravityMultiplier - 1) * Time.deltaTime;
-//         }
+        else if (rb.velocity.y > 0/* && !jumpRequest*/)
+        {
+            rb.velocity += Physics.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+
+    public void ResetJump()
+    {
+        jumpRequestTime = 0.0f;
+        jumpRequest = false;
     }
 
 }
--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -3,18 +3,56 @@
 public class Player : Controller
 {
     [Header("Input Settings")]
-    public string jumpButton = "Jump";
-    public string horizontalCtrl = "Horizontal";
-
-    // Start is called before the first frame update
-    void Start()
-    {
+    public string jumpButton = "Jump_P1";
+    public string horizontalCtrl = "Horizontal_P1";
         
-    }
-
-    // Update is called once per frame
-    void Update()
+    protected override void GetInput()
     {
-        
+        moveInput = Input.GetAxisRaw(horizontalCtrl);
+
+        if (moveInput != 0)
+        {
+            facingDirection = moveInput;
+        }
+
+        if (facingDirection >= 0.0f)
+        {
+            transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+        }
+
+        if (Input.GetButton(jumpButton))
+        {
+            if (jumpRequestTime == 0f)
+            {
+                if (isGrounded || jumpRequestTime < 1f && jumpCounter < 2)
+                {
+                    jumpRequest = true;
+                    jumpCounter++;
+                    isGrounded = false;
+                    jumpTimeCounter = jumpTime;
+                }
+            }
+
+            if (jumpRequestTime > 0f && jumpRequest)
+            {
+                if (jumpTimeCounter > 0)
+                {
+                    jumpTimeCounter -= Time.deltaTime;
+                }
+                else
+                {
+                    jumpRequest = false;
+                }
+            }
+            jumpRequestTime += Time.deltaTime;
+        }
+        else
+        {
+            ResetJump();
+        }
     }
 }
